d963f31e0376b30a173833627c543420e3fa0062
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-06-25 16:21:59 -0400

    added to index.html and app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,16 @@
-// Sorry, will get all of this done today!!! 
+//Ready document to allow submit button to allow user info submission
+$(document).ready(function () {
+    console.log("ready!");
 
+var topics = ["lion", "tiger", "bear"];
 
-// Setting up the Giphy API
-var queryURL =""
+for (let index = 0; index < topics.length; index++) {
+    const element = topics[index];
+    
+}
 
-//
+//On click for the Submit button
+$('#searchButton').on("click", function () {
+    console.log("click works =D");
+
+})}); 
